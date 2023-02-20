920632c6ecb25dedf21a008b29bc6a30082b8305
Mary Vang <mary_vang12@hotmail.com>
2023-02-20 05:25:58 -0500

    code

---
+++ 005/script.js
@@ -0,0 +1,13 @@
+/**
+ * here
+ * is
+ * another comment
+ * with like block thing
+ * usuallyat the top of a page
+ */
+
+/*
+more comment stuff
+*/
+// here is a comment in js
+alert("illustrating comments");
