1ce79338f7c6568f3f93b0088177b44fac3f2992
Mary Vang <mary_vang12@hotmail.com>
2023-03-16 04:44:30 -0400

    more on variables

---
+++ variables/main.js
@@ -0,0 +1,23 @@
+let myName = "59"
+let myAge = 59
+let myHappiness = true
+let newVar = 'SOMETHING'
+const myConst = 42
+console.log(myAge == myName)
+console.log(myAge === myName)
+
+let myVar
+console.log(`myVar is typeof ${typeof myVar}`)
+
+console.log(`myName is typeof ${typeof myName}`)
+console.log(`myName is value ${myName}`)
+console.log(`myAge is typeof ${typeof myName}`)
+console.log(`myHappiness is typeof ${typeof myHappiness}`)
+
+// don't use var
+// if (true) {
+//     const myConst = 43
+//     console.log{myConst}
+// } else {
+
+// }
